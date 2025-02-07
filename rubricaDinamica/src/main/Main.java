7d111006b7a1e2789945cd3fd08a82d2bb3a2019
Veronesi Niccolo <veronesi.niccolo@osdbsesto>
2025-02-07 10:47:02 +0100

    Rubrica_V1.1

---
+++ rubricaDinamica/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ rubricaDinamica/src/rubrica/Numero.java
@@ -0,0 +1,36 @@
+package rubrica;
+public class Numero {
+    private String telefono;
+    private String tipo;
+    public Numero(String telefono, int tipo){
+        this.telefono = telefono;
+        inizializzaTipo(tipo);
+    }
+    private void inizializzaTipo(int index){
+        tipo = Type.tipi[index];
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Numero{" +
+                "telefono='" + telefono + '\'' +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
+}
