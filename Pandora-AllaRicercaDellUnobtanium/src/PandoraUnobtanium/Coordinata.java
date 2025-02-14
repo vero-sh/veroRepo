4d81ee2cb5a0f5163e28e001f253cbbdb7bb5a4c
Veronesi Niccolo <veronesi.niccolo@osdbsesto>
2025-02-14 10:41:50 +0100

    Pandora_v1.1

--- Pandora-AllaRicercaDellUnobtanium/src/PandoraUnobtanium/Coordinata.java
+++ Pandora-AllaRicercaDellUnobtanium/src/PandoraUnobtanium/Coordinata.java
@@ -1,14 +1,53 @@
 package PandoraUnobtanium;
 
 public class Coordinata {
-    //------------------------------------------------------------------------------------------------------------------
-    //------------------------------------------------------ATTRIBUTI---------------------------------------------------
-    //------------------------------------------------------------------------------------------------------------------
 
-    
+    private double latitudine, longitudine;
+    private String puntiCardinaliLat, puntiCardinaliLong;
 
-    //------------------------------------------------------------------------------------------------------------------
-    //--------------------------------------------------------METODI----------------------------------------------------
-    //------------------------------------------------------------------------------------------------------------------
+    public Coordinata(double latitudine, double longitudine, String puntiCardinaliLat, String puntiCardinaliLong) {
+        this.latitudine = latitudine;
+        this.longitudine = longitudine;
+        this.puntiCardinaliLat = puntiCardinaliLat;
+        this.puntiCardinaliLong = puntiCardinaliLong;
+    }
 
+    public double getLatitudine() {
+        return latitudine;
+    }
+
+    public void setLatitudine(double latitudine) {
+        this.latitudine = latitudine;
+    }
+
+    public double getLongitudine() {
+        return longitudine;
+    }
+
+    public void setLongitudine(double longitudine) {
+        this.longitudine = longitudine;
+    }
+
+    public String getPuntiCardinaliLat() {
+        return puntiCardinaliLat;
+    }
+
+    public void setPuntiCardinaliLat(String puntiCardinaliLat) {
+        this.puntiCardinaliLat = puntiCardinaliLat;
+    }
+
+    public String getPuntiCardinaliLong() {
+        return puntiCardinaliLong;
+    }
+
+    public void setPuntiCardinaliLong(String puntiCardinaliLong) {
+        this.puntiCardinaliLong = puntiCardinaliLong;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate(" +
+                latitudine + puntiCardinaliLat + ","+
+                longitudine + puntiCardinaliLong + ")";
+    }
 }
