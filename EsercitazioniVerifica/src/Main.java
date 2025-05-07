74c9df3facdc14e5c272b8ba21d6d94366c38ec5
Veronesi Niccolo <veronesi.niccolo@osdbsesto>
2025-05-07 08:44:52 +0200

    EsercitazioneFile1_v1.0

---
+++ EsercitazioniVerifica/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String path = "X:\\Users\\veronesi.niccolo\\Documents\\progetti\\veroRepo\\EsercitazioniVerifica\\src\\input.txt";
+
+        try {
+            FileReader fileReader = new FileReader(path);
+            int carattere;
+
+            while ((carattere = fileReader.read()) != -1) {
+                System.out.print((char) carattere);
+            }
+        } catch (IOException e) {
+            System.err.println("Errore nella lettura del file: " + e.getMessage());
+        }
+    }
+}
