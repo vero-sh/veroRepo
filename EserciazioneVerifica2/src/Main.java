39bd6edf5aa1bd9a193f9658b5b9c96694f5ee4b
Veronesi Niccolo <veronesi.niccolo@osdbsesto>
2025-05-07 08:59:30 +0200

    EsercitazioneVerifica2_v1.0

---
+++ EserciazioneVerifica2/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String path = "X:\\Users\\veronesi.niccolo\\Documents\\progetti\\veroRepo\\EserciazioneVerifica2\\src\\input.txt";
+
+        try {
+            FileReader fileReader = new FileReader(path);
+            BufferedReader bf = new BufferedReader(fileReader);
+            String line = "";
+
+            while ((line = bf.readLine()) != null){
+                System.out.print(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Errore nella lettura del file: " + e.getMessage());
+        }
+    }
+}
