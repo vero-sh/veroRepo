ae14788ea2ae40ee5f232d13e28a0160ad261936
Niccolò <zizzi.veronesi@gmail.com>
2025-12-05 09:07:17 +0100

    Es2

--- WebDev/Esercizi/es2/script.js
+++ WebDev/Esercizi/es2/script.js
@@ -1,12 +1,109 @@
-//Crea due caselle di input per inserire due numeri e un bottone che, quando premuto, calcoli e mostri la loro somma sullo schermo usando getElementById e textContent.
+let btnLibera = document.getElementById('libera')
+let btnOccupa = document.getElementById('occupa')
+let btnElimina = document.getElementById('elimina')
+let boxAula = document.getElementById('aula')
+let inUtente = document.getElementById('utente')
+let inCod = document.getElementById('cod')
 
-let a = document.getElementById("a");
-let b = document.getElementById("b");
-let btn = document.getElementById("btn");
-let r = document.getElementById("result");
-let sum = 0;
+let arrayPc = [
+    {codicePc : "A01", utente : "", stato : "libero"},
+    {codicePc : "B02", utente : "", stato : "libero"},
+    {codicePc : "C03", utente : "", stato : "libero"}
+]
 
-btn.addEventListener("click", () => {
-    sum = Number(a.value) + Number(b.value);
-    r.textContent = sum;
-});
+function pcByCod (cod) {
+    for (let pc of arrayPc) {
+        if (cod == pc.codicePc) {
+            return pc
+        } 
+    }
+    alert('CODICE NON VALIDO')
+    return null
+}
+
+function pcByUtente (utente) {
+    for (let pc of arrayPc) {
+        if (utente == pc.utente) {
+            return pc
+        }
+    }
+    return null
+}
+
+function issetCod () {
+    if (inCod.value != "") {
+        return true
+    }
+    alert("INSERIRE UN CODICE")
+    return false
+}
+
+function issetUtente () {
+    if (inUtente.value != "") {
+        return true
+    }
+    alert("INSERIRE UN UTENTE")
+    return false
+}
+
+
+function visualizza () {
+    boxAula.innerHTML = ""
+    for (let pc of arrayPc) {
+        let div = document.createElement('div')
+        div.textContent = pc.codicePc + ": " + pc.stato
+        if (pc.stato == "occupato") {
+            div.textContent += " da " + pc.utente
+        }
+        boxAula.append(div)
+    }
+}
+
+function aggiornaArrayPc (pcToDelete) {
+    let newArrayPc = []
+    for (let pc of arrayPc) {
+        if (pc != pcToDelete) {
+            newArrayPc.push(pc)
+        }
+    }
+    arrayPc = newArrayPc
+}
+
+visualizza()
+
+
+btnOccupa.addEventListener("click", () => {
+    if (issetCod() && issetUtente()) {
+        let pc = pcByCod(inCod.value)
+        if (pc != null) {
+            if (pc.stato == "libero") {
+                pc.stato = "occupato"
+                pc.utente = inUtente.value
+                visualizza()
+            } else {
+                alert("PC GIA' OCCUPATO DA " + pc.utente)
+            }
+        }
+    }
+})
+
+btnLibera.addEventListener("click", () => {
+    if (issetCod()) {
+        let pc = pcByCod(inCod.value)
+        if(pc != null) {
+            pc.stato = "libero"
+            pc.utente = ""
+            visualizza()
+        }
+    }
+})
+
+btnElimina.addEventListener("click", () => {
+    if (issetCod()) {
+        let pc = pcByCod(inCod.value)
+        if (pc != null) {
+            aggiornaArrayPc(pc)
+            visualizza()
+        }
+    }
+})
